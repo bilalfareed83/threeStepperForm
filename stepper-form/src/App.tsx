4a91ff89cb8a799bbf499168d58d30ff5c656bda
bilalfareed83 <53287592+bilalfareed83@users.noreply.github.com>
2020-09-14 15:30:55 +0500

    first commit

---
+++ stepper-form/src/App.tsx
@@ -0,0 +1,14 @@
+import { Stepper } from "@material-ui/core";
+import React from "react";
+import "./App.css";
+import StepperForm from "./Stepper";
+
+const App = () => {
+  return (
+    <div className="App">
+      <StepperForm />
+    </div>
+  );
+};
+
+export default App;
